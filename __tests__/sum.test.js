c370fbdcea9da616bb831205612e08e31e054675
Victor Gazzinelli <vpezzigc@gmail.com>
2024-09-18 07:49:15 -0300

    Added new snapshot test without veryfying the snapshot to identify CI behaviour

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -3,3 +3,7 @@ const sum = require('../sum');
 test('snapshot test for sum function', () => {
     expect(sum(1, 2)).toMatchSnapshot();
 });
+
+test('other snapshot test for sum function', () => {
+    expect(sum(3, 4)).toMatchSnapshot();
+});
